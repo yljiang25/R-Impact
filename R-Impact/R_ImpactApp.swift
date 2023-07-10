d1c21dc4c74143525d7545bc9d0205fea4457369
Samuel2208 <108751960+Samuel2208@users.noreply.github.com>
2023-07-10 13:42:17 -0500

    Initial Commit

---
+++ R-Impact/R_ImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_ImpactApp.swift
+//  R-Impact
+//
+//  Created by Victor Escudero on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct R_ImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
